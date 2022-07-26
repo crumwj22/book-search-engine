4bbfe65a508eae6d07daa484e7578199e6f35659
crumwj22 <crumwj22@hotmail.com>
2022-07-25 17:18:58 -0700

    completed code for resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,63 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    getSingleUser: async ({ user = null, params }, res) => {
+      return User.findOne(
+        { _id: user ? user._id : params.id },
+        { username: params.username }
+      );
     },
   },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    createUser: async ({ body }, res) => {
+      const user = await User.create({ body }, res);
+      const token = signToken(user);
+
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
+    login: async ({ body }, res) => {
+      const user = await User.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async ({ user, body }, res) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+
+      return User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
+
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      // throw new AuthenticationError("You are not logged in!");
+    },
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    deleteBook: async ({ user, params }, res) => {
+      return User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
         { new: true }
       );
-      return vote;
     },
   },
 };
