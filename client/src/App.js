45785f935bdf709211c8ad26c4358c00023b8d46
crumwj22 <crumwj22@hotmail.com>
2022-07-27 22:30:12 -0700

    removed link from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import Home from "./pages/Home";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
