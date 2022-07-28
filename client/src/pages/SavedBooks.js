43692984910ac23ba2cb495d59a3b157085b4aeb
crumwj22 <crumwj22@hotmail.com>
2022-07-27 22:44:11 -0700

    added node modules, updated components

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -8,7 +8,7 @@ import {
 } from "react-bootstrap";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
